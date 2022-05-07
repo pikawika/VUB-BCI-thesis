6d45edc83dd4969a8fac0f4d5363ed8d9f9a5e3e
pikawika <lennertbontinck@live.be>
2022-05-07 21:35:24 +0200

    Shallow Conv Net configuration

--- code/utils/EEGModels.py
+++ code/utils/EEGModels.py
@@ -356,7 +356,16 @@ def log(x):
     return K.log(K.clip(x, min_value = 1e-7, max_value = 10000))   
 
 
-def ShallowConvNet(nb_classes, Chans = 64, Samples = 128, dropoutRate = 0.5):
+# Note: modified by Lennert Bontinck to allow for setting the pool_size, strides and conv filters
+# TODO: extra params!
+def ShallowConvNet(nb_classes,
+                   Chans = 64,
+                   Samples = 128,
+                   dropoutRate = 0.5,
+                   conv_filters = 13,
+                   strides = 7,
+                   pool_size = 35
+                   ):
     """ Keras implementation of the Shallow Convolutional Network as described
     in Schirrmeister et. al. (2017), Human Brain Mapping.
     
@@ -383,14 +392,14 @@ def ShallowConvNet(nb_classes, Chans = 64, Samples = 128, dropoutRate = 0.5):
 
     # start the model
     input_main   = Input((Chans, Samples, 1))
-    block1       = Conv2D(40, (1, 13), 
+    block1       = Conv2D(40, (1, conv_filters), 
                                  input_shape=(Chans, Samples, 1),
                                  kernel_constraint = max_norm(2., axis=(0,1,2)))(input_main)
     block1       = Conv2D(40, (Chans, 1), use_bias=False, 
                           kernel_constraint = max_norm(2., axis=(0,1,2)))(block1)
     block1       = BatchNormalization(epsilon=1e-05, momentum=0.9)(block1)
     block1       = Activation(square)(block1)
-    block1       = AveragePooling2D(pool_size=(1, 35), strides=(1, 7))(block1)
+    block1       = AveragePooling2D(pool_size=(1, pool_size), strides=(1, strides))(block1)
     block1       = Activation(log)(block1)
     block1       = Dropout(dropoutRate)(block1)
     flatten      = Flatten()(block1)
